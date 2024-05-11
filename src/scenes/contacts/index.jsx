470400b740982cb4e4117d8e4d520b51619051f6
Meryem-megdiche <147342991+Meryem-megdiche@users.noreply.github.com>
2024-05-11 16:55:18 +0100

    salem

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -20,7 +20,7 @@ const RfidScanner = ({ setRfid }) => {
                   const decoder = new TextDecoder();
                   for (const record of event.message.records) {
                       setRfid(decoder.decode(record.data));
-                      alert(decoder.decode(record.data));
+                      enqueueSnackbar(decoder.decode(record.data));
                   }
               };
           } catch (error) {
