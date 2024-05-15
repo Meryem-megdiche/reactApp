d0d01a87bdd5d335a55c868aeb7916d27f4d3fff
Meryem-megdiche <147342991+Meryem-megdiche@users.noreply.github.com>
2024-05-15 16:25:45 +0100

    laa

--- src/scenes/intervention/index.jsx
+++ src/scenes/intervention/index.jsx
@@ -78,19 +78,21 @@ const Intervention = () => {
 
     try {
       const response = await axios.post('https://nodeapp-0ome.onrender.com/api/interventions', dataToSubmit);
-      console.log("Réponse du serveur :", response.data); // Ajout du log pour la réponse du serveur
+      console.log("Réponse du serveur :", response.data);
       if (response.data.success) {
         setSuccessMessage("Intervention ajoutée avec succès");
+        setErrorMessage(null);
         setTimeout(() => navigate('/liste'), 800);
       } else {
         setErrorMessage(response.data.message || "Ajout réussi");
+        setSuccessMessage(null);
       }
     } catch (error) {
       console.error('Erreur lors de l\'ajout de l\'intervention :', error);
       setErrorMessage("Erreur côté client : " + error.message);
+      setSuccessMessage(null);
     }
   };
-
   const navigateToinvList = () => {
     navigate('/liste');
   };
