fbe6d53b17abc1a66e9d6fd6e1dc3e8c8120ca76
Meryem-megdiche <147342991+Meryem-megdiche@users.noreply.github.com>
2024-05-15 16:07:22 +0100

    lalhha

--- src/scenes/intervention/index.jsx
+++ src/scenes/intervention/index.jsx
@@ -102,11 +102,11 @@ const Intervention = () => {
                 type="text"
                 label="Nom de l'équipement"
                 name="equipmentName"
-                value={values.equipment}
+                value={values.equipmentName}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                error={touched.equipment && Boolean(errors.equipment)}
-                helperText={touched.equipment && errors.equipment}
+                error={touched.equipmentName && Boolean(errors.equipmentName)}
+                helperText={touched.equipmentName && errors.equipmentName}
                 sx={{ gridColumn: "span 4" }}
                 InputProps={{
                   readOnly: true,
