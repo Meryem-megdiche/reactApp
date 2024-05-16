1d266b779ae94bca679090a1c01491f36c4f1101
Meryem-megdiche <147342991+Meryem-megdiche@users.noreply.github.com>
2024-05-16 23:06:16 +0100

    newwwghgg

--- src/scenes/topologi/Inventory.jsx
+++ src/scenes/topologi/Inventory.jsx
@@ -56,7 +56,7 @@ const Topologi = () => {
             if (selectedEquipment) {
               selectedEquipment.ConnecteA.push(scannedEquipment._id);
               try {
-                await axios.put(`https://nodeapp-0ome.onrender.com/equip/${selectedEquipment._id}`, selectedEquipment);
+                await axios.put(`https://nodeapp-0ome.onrender.com/equip/equip/${selectedEquipment._id}`, selectedEquipment);
                 setAlertMessage(`Connexion créée entre ${selectedEquipment.Nom} et ${scannedEquipment.Nom}`);
               } catch (updateError) {
                 console.error('Error updating equipment:', updateError);
@@ -180,7 +180,7 @@ const Topologi = () => {
       const sourceEquipment = scannedEquipments.find(equip => equip._id === sourceId);
       if (sourceEquipment) {
         sourceEquipment.ConnecteA.push(targetId);
-        await axios.put(`https://nodeapp-0ome.onrender.com/equip/${sourceEquipment._id}`, sourceEquipment);
+        await axios.put(`https://nodeapp-0ome.onrender.com/equip/equip/${sourceEquipment._id}`, sourceEquipment);
         setAlertMessage(`Connexion créée entre ${sourceEquipment.Nom} et l'équipement cible.`);
         fetchScannedEquipments(); // Refresh the scanned equipments to reflect the new connection
       }
