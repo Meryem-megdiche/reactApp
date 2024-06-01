8f39ec3a42a35f33f062b3794ff197fa11718cf6
Meryem-megdiche <147342991+Meryem-megdiche@users.noreply.github.com>
2024-06-01 19:16:45 +0100

    new

--- src/components/barChart.jsx
+++ src/components/barChart.jsx
@@ -162,11 +162,11 @@ const getDateFormat = (startDate, endDate) => {
 
 const getColorByTTL = (TTL) => {
   console.log("Determining color for TTL:", TTL);
-  if (TTL < 48) {
+  if (TTL <= 46) {
     return "green";
-  } else if (TTL >= 48 && TTL <= 50) {
+  } else if (TTL > 46 && TTL <= 48) {
     return "orange";
-  } else if (TTL >= 113) {
+  } else if (TTL > 48 && TTL < 113) {
     return "red";
   } else {
     console.error("Unexpected TTL value:", TTL);
